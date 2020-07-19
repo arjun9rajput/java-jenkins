13855ae69ea310c2013bf533a0254d6f446fea27
arjun9rajput <arjun9rajput@gmail.com>
2020-07-19 16:37:43 +0530

    First commit

---
+++ src/Arjun1/MyClass.java
@@ -0,0 +1,10 @@
+package Arjun1;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+	 
+	       System.out.println("This is my first code");
+	       System.out.println("Welcome to Docker");
+	       }
+}
